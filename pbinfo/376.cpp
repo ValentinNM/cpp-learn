44a51a1e7d182637675e7848e3b1bd908d3c9c19
ValentinNM <valentinm.web@gmail.com>
2022-03-03 22:33:26 +0200

    Add sum of divisor

--- pbinfo/376.cpp
+++ pbinfo/376.cpp
@@ -1,21 +1,31 @@
 // Se citește un număr natural n.
 // Să se determine suma divizorilor săi.
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() { 
-
+int main()
+{
     int n, i;
     cin >> n;
     long long s = 0;
-    for(i = 1; i * i < n; i++){ 
-        if(n % i == 0) { 
-        s += n / i + i;
+    for (i = 1; i * i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            if (i % 2 != 0)
+            {
+                s += i;
+            }
+            if ((n / i) % 2 != 0)
+            {
+                s += n / i;
+            }
         }
     }
-    if(i * i == n) { 
-        s +=  i;
+    if (i * i == n && i % 2 != 0)
+    {           // cazul pentru patrat perfect
+        s += i;
     }
     cout << s;
     return 0;
